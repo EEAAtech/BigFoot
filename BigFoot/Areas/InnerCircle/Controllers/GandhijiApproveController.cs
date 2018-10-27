d5cfe5c6508cbcabfcb2e3d175dee956212b4fe6
93vpratham <v.pratham93@gmail.com>
2018-10-27 13:31:54 +0530

    Admin view created form user comments

---
+++ BigFoot/Areas/InnerCircle/Controllers/GandhijiApproveController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BigFoot;
+
+namespace BigFoot.Areas.InnerCircle.Controllers
+{
+    public class GandhijiApproveController : EAIController
+
+    {
+        private BigFootEntities db = new BigFootEntities();
+
+        // GET: InnerCircle/GandhijiApprove
+        public ActionResult Index()
+        {
+            return View(db.UserComments.ToList().OrderByDescending(c => c.Id));
+        }
+    }
+}
